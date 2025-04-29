cb52e0f21c6dc46ea6f159e72eac0048fec90586
njokinjuguna <ntikinjuguna@gmail.com>
2025-04-29 23:27:52 +0200

    Fix: add global types for SpeechRecognition again

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,23 +1,19 @@
-// src/global.d.ts
-
 declare global {
-    interface Window {
-      SpeechRecognition: typeof SpeechRecognition;
-      webkitSpeechRecognition: typeof SpeechRecognition;
-    }
-  
-    // If needed (for TS to accept SpeechRecognition)
-    var SpeechRecognition: {
-      prototype: SpeechRecognition;
-      new (): SpeechRecognition;
-    };
-  
-    var webkitSpeechRecognition: {
-      prototype: SpeechRecognition;
-      new (): SpeechRecognition;
-    };
+  interface Window {
+    SpeechRecognition: typeof SpeechRecognition;
+    webkitSpeechRecognition: typeof SpeechRecognition;
   }
-  
-  export {};
-  
-  
+
+  // These are only needed to trick TypeScript
+  const SpeechRecognition: {
+    prototype: SpeechRecognition;
+    new (): SpeechRecognition;
+  };
+
+  const webkitSpeechRecognition: {
+    prototype: SpeechRecognition;
+    new (): SpeechRecognition;
+  };
+}
+
+export {};
